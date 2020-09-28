c55cb591a89d51cb6dd9eee99e6fbbc9d3fc2865
Yi Liu <yi.liu@eclipse-foundation.org>
2020-09-28 08:17:46 -0400

    add filter of working groups, but can only add one
    Signed-off-by: Yi Liu <yi.liu@eclipse-foundation.org>

--- js/components/pastEvents/PastEventsList.js
+++ js/components/pastEvents/PastEventsList.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import EventCard from '../EventCard';
+import Loading from '../Loading';
 
-const PastEventsList = ({ events }) => {
-    return (
-        <div className="container">
-          <div className="row margin-bottom-20">
-            <div className="col-md-6">
-              {/* Filters will be here */}
-              Filters...
-            </div>
-    
-            <div className="col-md-18 event-list-wrapper">
-              {events.map((event) => (
-                <div className="col-md-12 max-min-width past-event-fade-in" key={event.id}>
-                  <EventCard event={event} />
-                </div>
-              ))}
-            </div>
+const PastEventsList = ({ events, isFetchingMore, fetchMore, paras }) => {
+
+  return (
+    <> 
+      <div className="col-md-18 event-list-wrapper">
+        {events.map((event) => (
+          <div className="col-md-12 max-min-width" key={event.id}>
+            <EventCard event={event} />
           </div>
+        ))}
+  
+        <div className="past-event-container">
+          { isFetchingMore && <Loading /> }
+          { !isFetchingMore && !paras && <button className="btn btn-primary" onClick={fetchMore}>Load More</button>}
+          { !isFetchingMore && paras && <button className="btn btn-primary" onClick={() => fetchMore(paras)}>Load More</button>}
         </div>
-      )
+      </div>
+
+    </>
+  )
 }
 
 export default PastEventsList
