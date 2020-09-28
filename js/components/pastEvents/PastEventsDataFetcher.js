568e174a3de7bdb7ae9a53394550930ff6311f52
Yi Liu <yi.liu@eclipse-foundation.org>
2020-09-28 08:17:46 -0400

    add filter of working groups, but can only add one
    Signed-off-by: Yi Liu <yi.liu@eclipse-foundation.org>

--- js/components/pastEvents/PastEventsDataFetcher.js
+++ js/components/pastEvents/PastEventsDataFetcher.js
@@ -3,6 +3,8 @@ import Loading from '../Loading';
 import { alertTypes } from '../AlertsEnum';
 import Alerts from '../Alerts';
 import PastEventsList from './PastEventsList';
+import PastEventsFilters from './PastEventsFilters';
+import { getSelectedItems } from '../EventHelpers';
 
 const PastEventsDataFetcher = () => {
 
@@ -12,30 +14,62 @@ const PastEventsDataFetcher = () => {
   const [currentPage, setCurrentPage] = useState(1)
   const [isFetchingMore, setIsFetchingMore] = useState(false)
 
+  const [checkedWorkingGroups, setCheckedWorkingGroups] = useState({})
+  const [checkedTypes, setCheckedTypes] = useState({})
+
+  const [dataWithFilters, setDataWithFilters] = useState([])
+
   const fetchingData = (page) => {
-    fetch(`https://newsroom.eclipse.org/api/events?page=${page}&pagesize=4`)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setLoading(false)
-          setIsFetchingMore(false)
-          setEvents([...events, ...result.events])
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setLoading(false)
-          setIsFetchingMore(false)
-          setError(error)
-        }
-      )
+    let url = `https://newsroom.eclipse.org/api/events?page=${page}&pagesize=6`
+    fetch(url)
+    .then((res) => res.json())
+    .then(
+      (result) => {
+        setLoading(false)
+        setIsFetchingMore(false)
+        setEvents([...events, ...result.events])
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        setLoading(false)
+        setIsFetchingMore(false)
+        setError(error)
+      }
+    )
+  }
+
+  const fetchingDataWithParas = (page, para) => {
+    let url = `https://newsroom.eclipse.org/api/events?page=${page}&pagesize=6&parameters[publish_to]=${para}`
+    fetch(url)
+    .then((res) => res.json())
+    .then(
+      (result) => {
+        setLoading(false)
+        setIsFetchingMore(false)
+        setDataWithFilters([...dataWithFilters, ...result.events])
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        setLoading(false)
+        setIsFetchingMore(false)
+        setError(error)
+      }
+    )
   }
   
   // only load once first time
   useEffect(() => {
-    fetchingData(currentPage)
-  }, [])
+    let selectedWorkingGroups = getSelectedItems(checkedWorkingGroups)
+    if (selectedWorkingGroups && selectedWorkingGroups.length > 0) {
+      fetchingDataWithParas(currentPage, selectedWorkingGroups[0])
+    } else {
+      fetchingData(currentPage)
+    }
+  }, [checkedWorkingGroups])
 
   // get more when click on the button
   const fetchMore = () => {
@@ -44,22 +78,33 @@ const PastEventsDataFetcher = () => {
     setCurrentPage(prev => prev + 1)
   }
 
-  if (loading) return <Loading />
-  if (error) return <Alerts alertType={alertTypes.ERROR} message={error.message} />
+  const fetchMoreWithParas = (para) => {
+    setIsFetchingMore(true)
+    fetchingDataWithParas((currentPage + 1), para)
+    setCurrentPage(prev => prev + 1)
+  }
 
+  if (error) return <Alerts alertType={alertTypes.ERROR} message={error.message} />
 
   return (
     <>
       <p>Past events showing</p>
-      <PastEventsList events={events} />
-
       <div className="container">
         <div className="row margin-bottom-20">
-          <div className="col-md-6"></div>
-          <div className="col-md-18 past-event-container">
-            { isFetchingMore && <Loading /> }
-            { !isFetchingMore && <button className="btn btn-primary" onClick={fetchMore}>Load More</button>}
+          <div className="col-md-6">
+            <PastEventsFilters
+              checkedTypes={checkedTypes}
+              setCheckedTypes={setCheckedTypes}
+            />
+            <PastEventsFilters
+              checkedWorkingGroups={checkedWorkingGroups}
+              setCheckedWorkingGroups={setCheckedWorkingGroups}
+            />
           </div>
+          { loading ? <Loading /> : 
+            (getSelectedItems(checkedWorkingGroups) && getSelectedItems(checkedWorkingGroups).length > 0) ? 
+            <PastEventsList events={dataWithFilters} isFetchingMore={isFetchingMore} fetchMore={fetchMoreWithParas} paras={getSelectedItems(checkedWorkingGroups)[0]} /> :
+            <PastEventsList events={events} isFetchingMore={isFetchingMore} fetchMore={fetchMore} />}
         </div>
       </div>
     </>
